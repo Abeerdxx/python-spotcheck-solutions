8424b4daa5980321bed655dcaaab2eecfe1e7ac0
jona <jona@elevation.ac>
2019-10-19 17:43:59 +0300

    sc1

---
+++ variables/sc1.py
@@ -0,0 +1,9 @@
+address = "P. Sherman, 42 Wallaby Way, Sydney"
+number_of_friends = 3
+
+print(address) # outputs: P. Sherman, 42 Wallaby Way, Sydney
+print("number_of_friends") # outputs: number_of_friends
+print(3) # outputs: 3
+print("address") # outputs: address
+
+# Remember! A *variable* is a placeholder value, while a string (inside quotes) is the value itself.
