4b8813277646c725f590cb772d80bf6b681a092d
jona <jona@elevation.ac>
2019-10-21 23:09:48 +0300

    sc1

---
+++ Loops/sc1.py
@@ -0,0 +1,5 @@
+investments = [3605, 5355, 1664, 3172, 436, 1916, 160, 3738, 2902, 4114, 3462, 1323, 2102, 4253, 1964, 4566]
+
+for investment in investments:
+    if investment < 1800:
+        print(investment)
