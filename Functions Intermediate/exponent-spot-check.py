7e06f57227adc223d663e2acb4739fc7267ce217
Margalit-Y <54045628+Margalit-Y@users.noreply.github.com>
2020-03-29 19:45:19 +0300

    Update exponent-spot-check.py

--- Functions Intermediate/exponent-spot-check.py
+++ Functions Intermediate/exponent-spot-check.py
@@ -6,7 +6,7 @@ def get_power_of(exponent):
 
 calc_power_of_2 = get_power_of(2)
 calc_power_of_4 = get_power_of(4)
-print(calc_power_of_2(5))    # 25
-print(calc_power_of_2(7))    # 49
-print(calc_power_of_4(2))    # 16
-print(calc_power_of_4(3))    # 81
+print(calc_power_of_2(5))    # 5^2 = 25
+print(calc_power_of_2(7))    # 7^2 = 49
+print(calc_power_of_4(2))    # 2^4 = 16
+print(calc_power_of_4(3))    # 3^4 = 81
