843ada61f11ac788078abba78205a58c629a4242
Margalit-Y <54045628+Margalit-Y@users.noreply.github.com>
2020-03-29 15:34:11 +0300

    Update unicode_table.py

--- Strings-and-Unicode/unicode_table.py
+++ Strings-and-Unicode/unicode_table.py
@@ -1,16 +1,6 @@
-def get_unicode_table(start, end):
+def print_unicode_table(start, end):
     for i in range(start, end + 1):
         print(f"{i}: {chr(i)}")
 
 
-get_unicode_table(200, 1000)
-
-
-"""
-Should handle the case of invalid range?
-No
-In the documentation of chr() is written:
-ValueError will be raised if i is outside that range.
-
-https://docs.python.org/3/library/functions.html#chr
-"""
+print_unicode_table(200, 1000)
